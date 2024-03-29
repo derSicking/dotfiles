afce35fe0fe63fb891f192906c163e4a703a3234
Frederik Sicking <frederik.sicking@gmail.com>
2024-03-29 02:18:36 +0100

    add surround

--- config/nvim/lua/frederik/plugins/treesitter.lua
+++ config/nvim/lua/frederik/plugins/treesitter.lua
@@ -1,10 +1,13 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 		},
 	},
 }
