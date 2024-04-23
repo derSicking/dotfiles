d3841dc819edd4b36dbd83df83b5ad4bea44c9e2
Frederik Sicking <frederik.sicking@gmail.com>
2024-04-23 15:39:42 +0200

    add: gr binding to go to references with lsp and quickfix

--- config/nvim/lua/frederik/lsp.lua
+++ config/nvim/lua/frederik/lsp.lua
@@ -7,6 +7,7 @@ local ltex_lang = "en-US"
 local on_attach = function()
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
 end
 
 return {
