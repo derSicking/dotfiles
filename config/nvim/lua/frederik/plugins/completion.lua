1829ff3f3e28e697566338d970b121128c729972
Frederik Sicking <frederik.sicking@gmail.com>
2024-04-28 12:02:26 +0200

    add: completion hint for function signatures

--- config/nvim/lua/frederik/plugins/completion.lua
+++ config/nvim/lua/frederik/plugins/completion.lua
@@ -5,6 +5,7 @@ return {
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
 			{ "hrsh7th/cmp-buffer" },
 			{ "hrsh7th/cmp-path" },
 			{ "hrsh7th/cmp-nvim-lua" },
@@ -21,6 +22,7 @@ return {
 					end,
 				},
 				sources = {
+					{ name = "nvim_lsp_signature_help" },
 					{ name = "path" },
 					{ name = "nvim_lsp" },
 					{ name = "nvim_lua" },
