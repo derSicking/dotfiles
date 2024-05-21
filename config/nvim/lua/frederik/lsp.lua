7d33b911fe893625addf1422f2248933927ce913
Frederik Sicking <frederik.sicking@gmail.com>
2024-05-21 13:51:36 +0200

    add: lsp-lines and bindings to navigate diagnostics

--- config/nvim/lua/frederik/lsp.lua
+++ config/nvim/lua/frederik/lsp.lua
@@ -8,6 +8,8 @@ local on_attach = function()
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+	vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
+	vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
 end
 
 return {
--- config/nvim/lua/frederik/plugins/lsp.lua
+++ config/nvim/lua/frederik/plugins/lsp.lua
@@ -1,25 +1,31 @@
+local lsp_lines_enabled = false
+
 return {
 	{ "folke/neodev.nvim", lazy = false, priority = 103, opts = {} },
 	{ "williamboman/mason.nvim", lazy = false, priority = 102, opts = {} },
-	{ "williamboman/mason-lspconfig.nvim", lazy = false, priority = 101, config = function ()
-		require("mason-lspconfig").setup({
-			handlers = require("frederik.lsp").handlers
-		})
-		end
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		priority = 101,
+		config = function()
+			require("mason-lspconfig").setup({
+				handlers = require("frederik.lsp").handlers,
+			})
+		end,
 	},
 	{ "neovim/nvim-lspconfig", lazy = false, priority = 100 },
 	{
 		"WhoIsSethDaniel/mason-tool-installer",
 		lazy = false,
 		priority = 99,
-		config = function ()
+		config = function()
 			require("mason-tool-installer").setup({
 				ensure_installed = require("frederik.lsp").ensure_installed,
 				auto_update = true,
 				run_on_start = true,
 				debounce_hours = 1,
 			})
-		end
+		end,
 	},
 	{
 		"smjonas/inc-rename.nvim",
@@ -33,7 +39,26 @@ return {
 		"aznhe21/actions-preview.nvim",
 		lazy = true,
 		keys = {
-			{ "<leader>f", function () require("actions-preview").code_actions() end, mode = { "n", "v" } },
+			{
+				"<leader>f",
+				function()
+					require("actions-preview").code_actions()
+				end,
+				mode = { "n", "v" },
+			},
+		},
+	},
+	{
+		"maan2003/lsp_lines.nvim",
+		opts = {},
+		keys = {
+			{
+				"<leader>dl",
+				function()
+					lsp_lines_enabled = not lsp_lines_enabled
+					vim.diagnostic.config({ virtual_lines = lsp_lines_enabled, virtual_text = not lsp_lines_enabled })
+				end,
+			},
 		},
 	},
 }
