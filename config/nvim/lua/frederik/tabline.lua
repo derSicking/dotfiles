7d71ab9be382de0467df8c0e864ccb0409ba74a3
Frederik Sicking <frederik.sicking@gmail.com>
2024-06-01 10:10:11 +0200

    add: support named tags and oil tags on tabline

--- config/nvim/lua/frederik/tabline.lua
+++ config/nvim/lua/frederik/tabline.lua
@@ -28,10 +28,25 @@ _G.tabline = function()
 	end
 	for i, tag in ipairs(tags) do
 		local is_current = current_tag and current_tag.path == tag.path
+		local tag_string = tag.name
+		if not tag_string or string.len(tag_string) == 0 then
+			tag_string = vim.fs.basename(tag.path)
+		end
+		if not tag_string or string.len(tag_string) == 0 and tag.path:sub(1, 6) == "oil://" then
+			local cwd = vim.loop.cwd()
+			if not cwd then
+				tag_string = tag.path:sub(7)
+			else
+				tag_string = tag.path:sub(string.len(cwd) + 7)
+			end
+		end
+		if not tag_string or string.len(tag_string) == 0 then
+			tag_string = tag.path
+		end
 		if is_current then
-			line = line .. "%#TablineSel#  " .. i .. " " .. vim.fs.basename(tag.path) .. "  %#Tabline#"
+			line = line .. "%#TablineSel#  " .. i .. " " .. tag_string .. "  %#Tabline#"
 		else
-			line = line .. "%#Tabline#  " .. i .. " " .. vim.fs.basename(tag.path) .. "  %#Tabline#"
+			line = line .. "%#Tabline#  " .. i .. " " .. tag_string .. "  %#Tabline#"
 		end
 	end
 	return line
