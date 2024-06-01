fefa94147b238832325ce6304d0819edcb02c79c
Frederik Sicking <frederik.sicking@gmail.com>
2024-06-01 16:27:15 +0200

    add: show non-grapple buffer names in tabline

--- config/nvim/lua/frederik/tabline.lua
+++ config/nvim/lua/frederik/tabline.lua
@@ -17,6 +17,22 @@ vim.api.nvim_create_autocmd("User", {
 	end,
 })
 
+local path_to_tab_name = function(path)
+	local name = vim.fs.basename(path)
+	if not name or string.len(name) == 0 and path:sub(1, 6) == "oil://" then
+		local cwd = vim.loop.cwd()
+		if not cwd then
+			name = path:sub(7)
+		else
+			name = path:sub(string.len(cwd) + 7)
+		end
+	end
+	if not name or string.len(name) == 0 then
+		name = path
+	end
+	return name
+end
+
 _G.tabline = function()
 	local current_tag = grapple.find({ path = vim.fn.bufname() })
 
@@ -30,18 +46,7 @@ _G.tabline = function()
 		local is_current = current_tag and current_tag.path == tag.path
 		local tag_string = tag.name
 		if not tag_string or string.len(tag_string) == 0 then
-			tag_string = vim.fs.basename(tag.path)
-		end
-		if not tag_string or string.len(tag_string) == 0 and tag.path:sub(1, 6) == "oil://" then
-			local cwd = vim.loop.cwd()
-			if not cwd then
-				tag_string = tag.path:sub(7)
-			else
-				tag_string = tag.path:sub(string.len(cwd) + 7)
-			end
-		end
-		if not tag_string or string.len(tag_string) == 0 then
-			tag_string = tag.path
+			tag_string = path_to_tab_name(tag.path)
 		end
 		if is_current then
 			line = line .. "%#TablineSel#  " .. i .. " " .. tag_string .. "  %#Tabline#"
@@ -49,5 +54,13 @@ _G.tabline = function()
 			line = line .. "%#Tabline#  " .. i .. " " .. tag_string .. "  %#Tabline#"
 		end
 	end
+
+	if not current_tag then
+		local cur_name = path_to_tab_name(vim.fn.bufname())
+		if cur_name and string.len(cur_name) > 0 then
+			line = line .. "%#Tabline#  %#TablineSel# %#TablineFill# " .. cur_name .. "  %#Tabline#"
+		end
+	end
+
 	return line
 end
