c8bf81e8557f7196d3043022d99ff792bc92b943
Frederik Sicking <frederik.sicking@gmail.com>
2024-06-04 22:27:38 +0200

    fix: change conform formatting to async
    my laptop is slow :(

--- config/nvim/lua/frederik/plugins/formatter.lua
+++ config/nvim/lua/frederik/plugins/formatter.lua
@@ -3,11 +3,8 @@ return {
 
 	config = function()
 		require("conform").setup({
-			-- log_level = vim.log.levels.DEBUG,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			log_level = vim.log.levels.DEBUG,
+			format_after_save = { lsp_fallback = true },
 		})
 		require("frederik.formatter")
 	end,
