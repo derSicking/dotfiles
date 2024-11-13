9620bfd8fe9387d9ad3671a28366a2008b272aa4
Frederik Sicking <frederik.sicking@gmail.com>
2024-11-13 19:39:41 +0100

    add pyenv

--- oh-my-zsh/plugins/frederik/frederik.plugin.zsh
+++ oh-my-zsh/plugins/frederik/frederik.plugin.zsh
@@ -1,5 +1,6 @@
 source ${0:A:h}/aliases.zsh
-source ${0:A:h}/env.zsh
 source ${0:A:h}/eza.zsh
 source ${0:A:h}/nvm.zsh
 source ${0:A:h}/keybinds.zsh
+source ${0:A:h}/pyenv.zsh
+source ${0:A:h}/env.zsh
