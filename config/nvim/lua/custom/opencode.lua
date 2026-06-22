6ec71c51feb5d19f3b0c43ee1c71aaf62a3de12a
Frederik Sicking <frederik.sicking@gmail.com>
2026-06-22 14:35:34 +0200

    change opencode keymap

--- config/nvim/lua/custom/opencode.lua
+++ config/nvim/lua/custom/opencode.lua
@@ -1,5 +1,5 @@
 if vim.env.TMUX then
-	vim.keymap.set("n", "<leader>oo", function()
+	vim.keymap.set("n", "<leader>o", function()
 		vim.fn.system("tmux split-window -h 'opencode --port'")
 	end, { desc = "Start opencode in tmux split" })
 end
