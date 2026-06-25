8717d4412614ad448e7823982f22aa62cf58b5a4
Frederik Sicking <frederik.sicking@gmail.com>
2026-06-25 15:13:27 +0200

    open opencode in full height split

--- config/nvim/lua/custom/opencode.lua
+++ config/nvim/lua/custom/opencode.lua
@@ -1,5 +1,5 @@
 if vim.env.TMUX then
 	vim.keymap.set("n", "<leader>o", function()
-		vim.fn.system("tmux split-window -h 'opencode --port'")
+		vim.fn.system("tmux split-window -h -f 'opencode --port'")
 	end, { desc = "Start opencode in tmux split" })
 end
